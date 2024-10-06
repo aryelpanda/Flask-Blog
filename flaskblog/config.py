ddfefbddb26a967551a42064211744a095b2a9da
Ariel Reshef <aryelpanda@gmail.com>
2024-10-06 15:20:57 +0300

    first commit

---
+++ flaskblog/config.py
@@ -0,0 +1,12 @@
+
+class Config:
+    SECRET_KEY ='2ae5e1dd9285d57d934243f032de30bc'
+    #DB
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///site2.db'
+
+    #email Server
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME =  'aryelpanda@gmail.com'
+    MAIL_PASSWORD =  'jkhd mnfa atnc jryz'
